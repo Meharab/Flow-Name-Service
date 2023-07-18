ebe23a46639818dee210205add04828cb6e8abd0
meharab <meharablatif@hotmail.com>
2023-07-18 11:32:22 +0600

    first commit

---
+++ cadence/transactions/setTestPrices.cdc
@@ -0,0 +1,17 @@
+import Domains from 0xa756d8cbc3a42629 // "../contracts/Domains.cdc"
+
+transaction() {
+    let registrar: &Domains.Registrar
+    prepare(account: AuthAccount) {
+        self.registrar = account.borrow<&Domains.Registrar>(from: Domains.RegistrarStoragePath)
+            ?? panic("Could not borrow Registrar")
+    }
+
+    execute {
+        var len = 1
+        while len < 11 {
+            self.registrar.setPrices(key: len, val: 0.000001)
+            len = len + 1
+        }
+    }
+}
---
+++ web/src/pages/index.js
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { getAllDomainInfos } from "../flow/scripts";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  // Create a state variable for all the DomainInfo structs
+  // Initialize it to an empty array
+  const [domainInfos, setDomainInfos] = useState([]);
+
+  // Load all the DomainInfo's by running the Cadence script
+  // when the page is loaded
+  useEffect(() => {
+    async function fetchDomains() {
+      const domains = await getAllDomainInfos();
+      setDomainInfos(domains);
+    }
+
+    fetchDomains();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Flow Name Service</title>
+        <meta name="description" content="Flow Name Service" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+        <h1>All Registered Domains</h1>
+
+        <div className={styles.domainsContainer}>
+          {
+            // If no domains were found, display a message highlighting that
+            domainInfos.length === 0 ? (
+            <p>No FNS Domains have been registered yet</p>
+          ) : (
+            // Otherwise, loop over the array, and render information
+            // about each domain
+            domainInfos.map((di, idx) => (
+              <div className={styles.domainInfo} key={idx}>
+                <p>
+                  {di.id} - {di.name}
+                </p>
+                <p>Owner: {di.owner}</p>
+                <p>Linked Address: {di.address ? di.address : "None"}</p>
+                <p>Bio: {di.bio ? di.bio : "None"}</p>
+                {/* Parse the timestamps as human-readable dates */}
+                <p>
+                  Created At:{" "}
+                  {new Date(parseInt(di.createdAt) * 1000).toLocaleDateString()}
+                </p>
+                <p>
+                  Expires At:{" "}
+                  {new Date(parseInt(di.expiresAt) * 1000).toLocaleDateString()}
+                </p>
+              </div>
+            ))
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
